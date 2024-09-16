2e30942cc850bcde160c86f5e82aa0ed9613ad1f
Kay <kumartanmay315@gmail.com>
2024-09-16 20:53:02 +0100

    menu button

---
+++ components/global/menuButton.tsx
@@ -0,0 +1,14 @@
+const MenuButton = ({}) => (
+  <div className="menu-btn">
+    <div className="text-overflow is-menu h-effect">
+      <div className="menu-btn_text h-item">
+        <span className="word">Menu</span>
+      </div>
+      <div className="menu-btn_text h-item">
+        <span className="word">Menu</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default MenuButton;
