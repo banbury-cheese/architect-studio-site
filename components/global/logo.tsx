0b7c2bdba9e6a4cf98fbef8b91c605330648bdf1
Kay <kumartanmay315@gmail.com>
2024-09-16 20:54:14 +0100

    logo

--- components/global/logo.tsx
+++ components/global/logo.tsx
@@ -1,17 +1,30 @@
 const Logo = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="137.6"
-    height="94.375"
-    viewBox="0 0 137.6 94.375"
+  <a
+    t-white=""
+    // pload=""
+    href="/"
+    aria-current="page"
+    className="head-logo_holder w-inline-block w--current"
+    // style="clip-path: polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%); opacity: 1;"
   >
-    <path
-      d="M92.375,135.6V112.912L0,53.955V0H92.375V22.157L0,82.16V135.6Z"
-      transform="translate(1 93.375) rotate(-90)"
-      stroke="#000"
-      stroke-width="2"
-    />
-  </svg>
+    <div className="head-logo_height"></div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="137.6"
+      height="94.375"
+      viewBox="0 0 137.6 94.375"
+      className="head-logo"
+    >
+      <path
+        id="Path_56"
+        data-name="Path 56"
+        d="M92.375,135.6V112.912L0,53.955V0H92.375V22.157L0,82.16V135.6Z"
+        transform="translate(1 93.375) rotate(-90)"
+        stroke="#000"
+        stroke-width="2"
+      />
+    </svg>
+  </a>
 );
 
 export default Logo;
