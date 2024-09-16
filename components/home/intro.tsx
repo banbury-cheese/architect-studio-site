94bf326b67e3f569584a48c671f4e7662d8acf1e
Kay <kumartanmay315@gmail.com>
2024-09-16 20:57:34 +0100

    home intro component

---
+++ components/home/intro.tsx
@@ -0,0 +1,29 @@
+const HomeIntroSection: React.FC = () => (
+  <div className="home-intro_section">
+    <div className="container-box">
+      <div className="home-intro_layout">
+        <div className="home-intro_left">
+          <div className="body-copy">
+            a120/03
+            <br />
+            SP_a
+          </div>
+        </div>
+        <div className="home-intro_right">
+          <h2 className="h-medium">
+            Our architecture practice fosters connection and grows relationships
+            between people, society and the environment. Our rigorous creative
+            process ensures we evolve not only what is made, but how ideas are
+            generated, evaluated and realised. Since 2003, our team has grown
+            across disciplines and thrives on collaboration. Driven by curiosity
+            and ambition, we believe that any enduring solution needs to
+            synchronise cogent analysis with sensitivity to the finer contextual
+            impact.
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export { HomeIntroSection };
